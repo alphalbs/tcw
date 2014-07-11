5703ba6fdf8297b0778cc527bdc82323d3623ac2
Robert Baruck <robert.baruck@gmail.com>
2014-07-11 20:32:20 +0200

    Add tooltips

--- levels/menu/menu.js
+++ levels/menu/menu.js
@@ -1,13 +1,48 @@
 $(document).ready(function(){
 
-// backgroud
+// tooltips
+	$('.menuButton').tooltip({
+		show: {
+			effect: "drop"
+		},
+		position: {
+			at: "right top"
+		}
+	});
+
+// backgroud and tooltip updates
 
 	// holy guacamole is this ugly .. model, view, controller would be so nice
-	if(window.parent.global.level_unlocked == 1) $('body').attr('style','background:url(../../images/level1.jpg) no-repeat');
-	else if(window.parent.global.level_unlocked == 2) $('body').attr('style','background:url(../../images/level2.jpg) no-repeat');
-	else if(window.parent.global.level_unlocked == 3) $('body').attr('style','background:url(../../images/level3.jpg) no-repeat');
-	else if(window.parent.global.level_unlocked == 4) $('body').attr('style','background:url(../../images/level4.jpg) no-repeat');
-	else if(window.parent.global.level_unlocked == 5) $('body').attr('style','background:url(../../images/level5.jpg) no-repeat');
+	if(window.parent.global.level_unlocked == 1) {
+		$('body').attr('style','background:url(../../images/level1.jpg) no-repeat');
+		$('#einkaufen').tooltip("option", "content", "Medallie: " + getMedal("lvl1") + "<br>Punkte: " + getPoints("lvl1") + " von 100");
+	}
+	else if(window.parent.global.level_unlocked == 2) {
+		$('body').attr('style','background:url(../../images/level2.jpg) no-repeat');
+		$('#einkaufen').tooltip("option", "content", "Medallie: " + getMedal("lvl1") + "<br>Punkte: " + getPoints("lvl1") + " von 100");
+		$('#entsorgung').tooltip("option", "content", "Medallie: " + getMedal("lvl2") + "<br>Punkte: " + getPoints("lvl2") + " von 100");
+	}
+	else if(window.parent.global.level_unlocked == 3) {
+		$('body').attr('style','background:url(../../images/level3.jpg) no-repeat');
+		$('#einkaufen').tooltip("option", "content", "Medallie: " + getMedal("lvl1") + "<br>Punkte: " + getPoints("lvl1") + " von 100");
+		$('#entsorgung').tooltip("option", "content", "Medallie: " + getMedal("lvl2") + "<br>Punkte: " + getPoints("lvl2") + " von 100");
+		$('#abholung').tooltip("option", "content", "Medallie: " + getMedal("lvl3") + "<br>Punkte: " + getPoints("lvl3") + " von 100");
+	}
+	else if(window.parent.global.level_unlocked == 4) {
+		$('body').attr('style','background:url(../../images/level4.jpg) no-repeat');
+		$('#einkaufen').tooltip("option", "content", "Medallie: " + getMedal("lvl1") + "<br>Punkte: " + getPoints("lvl1") + " von 100");
+		$('#entsorgung').tooltip("option", "content", "Medallie: " + getMedal("lvl2") + "<br>Punkte: " + getPoints("lvl2") + " von 100");
+		$('#abholung').tooltip("option", "content", "Medallie: " + getMedal("lvl3") + "<br>Punkte: " + getPoints("lvl3") + " von 100");
+		$('#verarbeitung').tooltip("option", "content", "Medallie: " + getMedal("lvl4") + "<br>Punkte: " + getPoints("lvl4") + " von 100");
+	}
+	else if(window.parent.global.level_unlocked == 5) {
+		$('body').attr('style','background:url(../../images/level5.jpg) no-repeat');
+		$('#einkaufen').tooltip("option", "content", "Medallie: " + getMedal("lvl1") + "<br>Punkte: " + getPoints("lvl1") + " von 100");
+		$('#entsorgung').tooltip("option", "content", "Medallie: " + getMedal("lvl2") + "<br>Punkte: " + getPoints("lvl2") + " von 100");
+		$('#abholung').tooltip("option", "content", "Medallie: " + getMedal("lvl3") + "<br>Punkte: " + getPoints("lvl3") + " von 100");
+		$('#verarbeitung').tooltip("option", "content", "Medallie: " + getMedal("lvl4") + "<br>Punkte: " + getPoints("lvl4") + " von 100");
+		$('#recycling').tooltip("option", "content", "Medallie: " + getMedal("lvl5") + "<br>Punkte: " + getPoints("lvl5") + " von 100");
+	}
 
 // game
 	window.parent.global.game = null;
@@ -16,6 +51,31 @@ $(document).ready(function(){
 	window.parent.global.score.lvl3_temp = 0;
 	var score = window.parent.global.score;
 	var total_score = score.lvl1 + score.lvl2 + score.lvl3 + score.lvl4 + score.lvl5;
+
+// helper functions
+
+	function getMedal (level) {
+		var score;
+		if(level == "lvl1") score = window.parent.global.score.lvl1;
+		else if(level == "lvl2") score = window.parent.global.score.lvl2;
+		else if(level == "lvl3") score = window.parent.global.score.lvl3;
+		else if(level == "lvl4") score = window.parent.global.score.lvl4;
+		else if(level == "lvl5") score = window.parent.global.score.lvl5;
+		if(score >= 100) return "Gold";
+		if(score >= 66) return "Silber";
+		if(score >= 33) return "Bronze";
+		return "keine";
+	}
+
+	function getPoints (level) {
+		var score;
+		if(level == "lvl1") score = window.parent.global.score.lvl1;
+		else if(level == "lvl2") score = window.parent.global.score.lvl2;
+		else if(level == "lvl3") score = window.parent.global.score.lvl3;
+		else if(level == "lvl4") score = window.parent.global.score.lvl4;
+		else if(level == "lvl5") score = window.parent.global.score.lvl5;
+		return score;
+	}
 
 // progress bar
 	window.parent.$('#progressbar').html("total points = " + total_score + " of " + window.parent.global.score.max);
